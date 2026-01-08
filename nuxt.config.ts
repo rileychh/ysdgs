16c018df073af6c6a7ae494921c58af00261eed3
Riley Ho <riley@rileychh.dev>
2026-01-09 01:54:51 +0800

    chore: disable Nuxt auto-imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  imports: {
+    scan: false,
+  },
+
   typescript: {
     typeCheck: 'build',
   },
