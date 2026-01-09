10775c598298f63faaede26e4d1273a5cb5e8b03
Riley Ho <riley@rileychh.dev>
2026-01-09 17:54:49 +0800

    feat: add account selector with user profile dropdown in header

---
+++ app/components/ui/avatar/Avatar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { AvatarRoot } from 'reka-ui'
+import { cn } from '~/utils/shadcn'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <AvatarRoot
+    :class="cn('relative flex size-8 shrink-0 overflow-hidden rounded-full', props.class)"
+    data-slot="avatar"
+  >
+    <slot />
+  </AvatarRoot>
+</template>
---
+++ app/components/ui/avatar/AvatarImage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { AvatarImageProps } from 'reka-ui'
+import { AvatarImage } from 'reka-ui'
+
+const props = defineProps<AvatarImageProps>()
+</script>
+
+<template>
+  <AvatarImage
+    v-bind="props"
+    class="aspect-square size-full"
+    data-slot="avatar-image"
+  >
+    <slot />
+  </AvatarImage>
+</template>
